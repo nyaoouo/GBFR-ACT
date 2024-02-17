4b473dcfc0ba734f7bcf8eb34e93fb6365bbc716
ny <1>
2024-02-18 02:34:51 +0800

    🔄 Add uac_start.cmd script for handling Windows UAC elevation when running Python.

---
+++ uac_start.cmd
@@ -0,0 +1,21 @@
+ @echo off
+setlocal enabledelayedexpansion
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% >nul 2>&1 || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~dp0"" && ""%~s0"" !params!", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+for /f "delims=" %%i in ('where python') do (
+    for /f "delims=" %%v in ('"%%i" -VV') do (
+        set "output=%%v"
+        !output!
+        echo !output! | findstr /C:"3.11" >nul && echo !output! | findstr /C:"64 bit" >nul
+        if !errorlevel! equ 0 (
+            echo Found Python 3.11 64-bit at %%i
+            "%%i" "act_ws.py"
+            goto :end
+        )
+    )
+)
+
+:end
+endlocal
+exit
