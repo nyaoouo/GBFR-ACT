0a33579c902941e9abdaa00e194f387f0eb927e1
ny <1>
2024-03-14 15:29:08 +0800

    🔧 Improve error message to include pattern that was not found or is not unique in injector.py

--- injector.py
+++ injector.py
@@ -552,12 +552,12 @@ class IPatternScanner:
         try:
             res = next(s)
         except StopIteration:
-            raise KeyError('pattern not found')
+            raise KeyError(f'pattern {pattern} not found')
         try:
             next(s)
         except StopIteration:
             return res
-        raise KeyError('pattern is not unique, at least 2 is found')
+        raise KeyError(f'pattern {pattern} is not unique, at least 2 is found')
 
     def find_addresses(self, pattern: str | Pattern):
         for address, _ in self.search(pattern):
