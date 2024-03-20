95dfe9b7ad0a9f8b31621a9e79a9e571ea36c87b
ny <64143453+nyaoouo@users.noreply.github.com>
2024-03-20 11:27:39 +0800

    🔧 Update mouse position using screen scale factor.

--- webview/main.py
+++ webview/main.py
@@ -7,9 +7,19 @@ import webview
 from webview.window import FixPoint
 
 
-def get_mouse_position() -> tuple[int, int]:
+def get_mouse_pos() -> tuple[int, int]:
     ctypes.windll.user32.GetCursorPos(ctypes.byref(point := ctypes.wintypes.POINT()))
     return point.x, point.y
+
+
+def get_mouse_info() -> tuple[tuple[int, int], tuple[float, float]]:
+    if not ctypes.windll.user32.GetCursorPos(ctypes.byref(point := ctypes.wintypes.POINT())): raise ctypes.WinError()
+    hmontor = ctypes.windll.user32.MonitorFromPoint(point, 2)
+    if not hmontor: raise ctypes.WinError()
+    dpi_x = ctypes.wintypes.UINT(1)
+    dpi_y = ctypes.wintypes.UINT(1)
+    ctypes.windll.shcore.GetDpiForMonitor(hmontor, 0, ctypes.byref(dpi_x), ctypes.byref(dpi_y))
+    return (point.x, point.y), (dpi_x.value / 100, dpi_y.value / 100)
 
 
 class Api:
@@ -25,7 +35,7 @@ class Api:
 
     def window_resize_start(self, direction):
         self.resize_direction = direction
-        self.resize_start = get_mouse_position()
+        self.resize_start = get_mouse_pos()
         self.resize_size = self._window.width, self._window.height
 
     def window_resize_end(self):
@@ -38,7 +48,7 @@ class Api:
         if not self.resize_start: return
         width, height = self.resize_size
         start_x, start_y = self.resize_start
-        x, y = get_mouse_position()
+        x, y = get_mouse_pos()
         delta_x, delta_y = x - start_x, y - start_y
         match self.resize_direction:
             case 'left':
@@ -74,8 +84,8 @@ class Api:
         self._window.resize(width, height, fix)
 
     def window_move_start(self):
-        self.move_start = get_mouse_position()
-        self.move_position = self._window.x, self._window.y
+        self.move_start, (scale_x, scale_y) = get_mouse_info()
+        self.move_position = self._window.x / scale_x, self._window.y / scale_y
 
     def window_move_end(self):
         self.move_start = None
@@ -84,8 +94,8 @@ class Api:
     def window_move_update(self):
         if not self.move_start: return
         start_x, start_y = self.move_start
-        x, y = get_mouse_position()
-        delta_x, delta_y = x - start_x, y - start_y
+        (x, y), (scale_x, scale_y) = get_mouse_info()
+        delta_x, delta_y = (x - start_x) / scale_x, (y - start_y) / scale_y
         self._window.move(self.move_position[0] + delta_x, self.move_position[1] + delta_y)
 
     def window_close(self):
@@ -98,16 +108,18 @@ class Api:
 def main():
     current_dir = os.path.dirname(sys.executable if getattr(sys, 'frozen', False) else __file__)
     if current_dir not in sys.path: sys.path.insert(0, current_dir)
-    os.chdir(current_dir)
 
     args = argparse.ArgumentParser()
     args.add_argument('--debug', action='store_true', default=False)
-    args.add_argument('url', nargs='?', default=f'{current_dir}/act_ws.html?isWebView=1')
+    args.add_argument('url', nargs='?', default=f'{current_dir}/act_ws.html')
     args = args.parse_args()
+
+    webpage_dir = os.path.dirname(args.url)
+    os.chdir(webpage_dir)
 
     api = Api()
     api._window = webview.create_window(
-        'act_ws', args.url,
+        'act_ws', args.url + '?isWebView=1',
         easy_drag=False, frameless=True, js_api=api, on_top=True
     )
     webview.start(
